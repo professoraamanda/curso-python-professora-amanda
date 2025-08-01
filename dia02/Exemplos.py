70e90bf9886e06c0c2661ad01d105967a0fc6eb5
Professora Amanda <amanda.xavier@pe.senac.br>
2025-08-01 12:30:37 -0300

    Exemplos.py

---
+++ dia02/Exemplos.py
@@ -0,0 +1,12 @@
+# Exemplo 1 - Primeiro programa
+print("Olá mundo!")
+
+# Exemplo 2 - Saudação personalizada
+nome = input("Digite seu nome: ")  # Entrada
+print("Seja bem-vindo(a),", nome)  # Saída
+
+# Exemplo 3 - Cálculo de média
+nota1 = float(input("Digite sua primeira nota: "))  # Entrada
+nota2 = float(input("Digite sua segunda nota: "))   # Entrada
+media = (nota1 + nota2) / 2                         # Processamento
+print("Sua média é:", media)                        # Saída
