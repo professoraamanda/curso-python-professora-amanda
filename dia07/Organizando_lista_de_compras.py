f592473c24d7c6f075e7448b8c50eee676bc31a8
Professora Amanda <professoraamanda84@gmail.com>
2025-08-22 10:03:33 -0300

    Update Organizando_lista_de_compras.py

--- dia07/Organizando_lista_de_compras.py
+++ dia07/Organizando_lista_de_compras.py
@@ -1,1 +1,17 @@
+#Aula 07 - Automatizando lista de compras
 
+lista = [] #isto é uma lista em python
+
+print("Digite os itens da sua lista de compras: ")
+print("Quando terminar digite 'sair': ")
+
+while True:
+  item = input("item: ")
+  if item == "sair":
+    break
+  else:
+    lista.append(item) #função da estrutura lista (anexar na lista)
+print("Sua lista de compras: \n")
+
+for produto in lista:
+  print(" - ",produto)
